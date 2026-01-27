02c2860532e1c52e98884cf5eab41b41cc3ccb7f
Harry Wadley <harryjwadley@gmail.com>
2026-01-26 17:55:41 -0700

    change agen

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ function showFeed() {
   feedBtn.classList.add("active");
   surveyBtn.classList.remove("active");
 
-  pageContent.classList.remove("survey-page");
-  pageContent.classList.add("feed-page");
+  document.body.style.background = "var(--royal-blue)";
 
   pageContent.innerHTML = `
     <h1>Feed Page</h1>
-    <p>This is the green feed page.</p>
+    <p>This is the blue feed page.</p>
   `;
 }
 
@@ -19,12 +18,11 @@ function showSurvey() {
   surveyBtn.classList.add("active");
   feedBtn.classList.remove("active");
 
-  pageContent.classList.remove("feed-page");
-  pageContent.classList.add("survey-page");
+  document.body.style.background = "var(--sea-green)";
 
   pageContent.innerHTML = `
     <h1>Survey Page</h1>
-    <p>This is the blue survey page.</p>
+    <p>This is the green survey page.</p>
   `;
 }
 
